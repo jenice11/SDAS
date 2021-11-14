f313a9bf2dc0108088931b43ee1c688c1faea25a
jenice11 <38397984+jenice11@users.noreply.github.com>
2021-11-14 20:57:40 +0800

    0.1 - Alpha
    Current functions:
    Registration
    Login
    Firebase
    Location upload
    
    Work in progress:
    Calculate distance between 2 coordinates

--- app/src/main/java/com/example/trackingapp/HomeActivity.java
+++ app/src/main/java/com/example/trackingapp/HomeActivity.java
@@ -1,60 +1,50 @@
 package com.example.trackingapp;
 
+import static android.content.ContentValues.TAG;
+
 import android.Manifest;
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Typeface;
 import android.location.Location;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.widget.Toolbar;
-
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.trackingapp.Interface.IFirebaseLoadDone;
-import com.example.trackingapp.Interface.IRecyclerItemClickListener;
+import com.example.trackingapp.Model.MyLocation;
 import com.example.trackingapp.Model.User;
-import com.example.trackingapp.Service.MyLocationReceiver;
 import com.example.trackingapp.Service.TrackingService;
 import com.example.trackingapp.Utils.Common;
 import com.example.trackingapp.ViewHolder.UserViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.android.gms.common.internal.Constants;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.mancj.materialsearchbar.MaterialSearchBar;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.google.firebase.database.ValueEventListener;
+import com.google.maps.android.SphericalUtil;
 
 
-public class HomeActivity extends AppCompatActivity implements IFirebaseLoadDone , View.OnClickListener{
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
 
     DrawerLayout mDrawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
@@ -64,6 +54,10 @@ public class HomeActivity extends AppCompatActivity implements IFirebaseLoadDone
     ImageView navprofile;
     Toolbar toolbar;
     private Button mTrackButton,mStopButton;
+    Double distance;
+    DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+
 
     /////////////
 
@@ -71,8 +65,7 @@ public class HomeActivity extends AppCompatActivity implements IFirebaseLoadDone
     RecyclerView recycler_friend_list;
     IFirebaseLoadDone firebaseLoadDone;
 
-    MaterialSearchBar searchBar;
-    List<String> suggestList = new ArrayList<>();
+//    MaterialSearchBar searchBar;
 
 
 
@@ -103,7 +96,6 @@ public class HomeActivity extends AppCompatActivity implements IFirebaseLoadDone
                 new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION ,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION},
                 1);
 
-
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
@@ -122,65 +114,10 @@ public class HomeActivity extends AppCompatActivity implements IFirebaseLoadDone
                         break;
                 }
                 return false;
+
             }
         });
 
-
-//        searchBar = findViewById(R.id.material_search_bar);
-//        searchBar.setCardViewElevation(10);
-//        searchBar.addTextChangeListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence s, int start, int before, int count) {
-//                List<String> suggest = new ArrayList<>();
-//                for (String search : suggestList) {
-//                    if (search.toLowerCase().contains(searchBar.getText().toLowerCase()))
-//                        suggest.add(search);
-//                }
-//                searchBar.setLastSuggestions(suggest);
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable editable) {
-//
-//            }
-//        });
-//
-//        searchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
-//            @Override
-//            public void onSearchStateChanged(boolean enabled) {
-//                if (!enabled) {
-//                    if (adapter != null) {
-//                        recycler_friend_list.setAdapter(adapter);
-//                    }
-//                }
-//            }
-//
-//            @Override
-//            public void onSearchConfirmed(CharSequence text) {
-//                startSearch(text.toString());
-//            }
-//
-//            @Override
-//            public void onButtonClicked(int buttonCode) {
-//
-//            }
-//        });
-//
-//        recycler_friend_list = findViewById(R.id.recycler_all_people);
-//        recycler_friend_list.setHasFixedSize(true);
-//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-//        recycler_friend_list.setLayoutManager(layoutManager);
-//        recycler_friend_list.addItemDecoration(new DividerItemDecoration(this, ((LinearLayoutManager) layoutManager).getOrientation()));
-//
-//        firebaseLoadDone = this;
-
-//        loadFriendList();
-//        loadSearchData();
     }
 
     public void onClick(View v) {
@@ -195,7 +132,7 @@ public class HomeActivity extends AppCompatActivity implements IFirebaseLoadDone
 
 //            startService(new Intent(this, TrackingService.class));
 //            Toast.makeText(this, "Start tracking now", Toast.LENGTH_SHORT).show();
-
+//            coordinates();
         }
 
         if(R.id.stopButton == v.getId()){
@@ -207,116 +144,56 @@ public class HomeActivity extends AppCompatActivity implements IFirebaseLoadDone
 
 
 
+//    private void coordinates() {
+//        mDatabase.child("users").child(mAuth.getCurrentUser()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DataSnapshot> task) {
+//                if (!task.isSuccessful()) {
+//                    Log.e("firebase", "Error getting data", task.getException());
+//                }
+//                else {
+//                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+//                }
+//            }
+//        })
+//
+//        Location locationA = new Location("point A");
+//
+//        locationA.setLatitude(latA);
+//        locationA.setLongitude(lngA);
+//
+//        Location locationB = new Location("point B");
+//
+//        locationB.setLatitude(latB);
+//        locationB.setLongitude(lngB);
+//
+//        float distance = locationA.distanceTo(locationB);
+//
+//        // Attach a listener to read the data at our posts reference
+//        mDatabase.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                Post post = dataSnapshot.getValue(HomeActivity.class);
+//                System.out.println(post);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                System.out.println("The read failed: " + databaseError.getCode());
+//            }
+//        });
+//
+//    }
+//
+//    public static void distanceBetween (double startLatitude, double startLongitude, double endLatitude, double endLongitude, float[] results){
+//
+//    }
+
+
+
 //    private void calculateDistance() {
 //        float[] results = new float[3];
 //        Location.distanceBetween(self.latitude, self.longitude, position.latitude, position.longitude, results);
 //        b.putString("distance_in_meters", String.valueOf(results[0]));
 //    }
-
-//    private void loadFriendList() {
-//        Query query = FirebaseDatabase.getInstance()
-//                .getReference(Common.USER_INFORMATION)
-//                .child(Common.loggedUser.getUid())
-//                .child(Common.ACCEPT_LIST);
-//
-//        FirebaseRecyclerOptions<User> options = new FirebaseRecyclerOptions.Builder<User>()
-//                .setQuery(query, User.class)
-//                .build();
-//
-//        adapter = new FirebaseRecyclerAdapter<User, UserViewHolder>(options) {
-//            @Override
-//            protected void onBindViewHolder(@NonNull UserViewHolder holder, int position, @NonNull final User model) {
-//                holder.txt_user_email.setText(new StringBuilder(model.getEmail()));
-//
-//                holder.setiRecyclerItemClickListener(new IRecyclerItemClickListener() {
-//                    @Override
-//                    public void onItemClickListener(View view, int position) {
-//                        Common.trackingUser = model;
-//                        startActivity(new Intent(HomeActivity.this, TrackingActivity.class));
-//
-//                    }
-//                });
-//            }
-//
-//            @NonNull
-//            @Override
-//            public UserViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-//                View itemView = LayoutInflater.from(viewGroup.getContext())
-//                        .inflate(R.layout.layout_user, viewGroup, false);
-//                return new UserViewHolder(itemView);
-//            }
-//        };
-//        adapter.startListening();
-//        ;
-//        recycler_friend_list.setAdapter(adapter);
-//    }
-//
-//    @Override
-//    protected void onStop() {
-//        if (adapter != null)
-//            adapter.stopListening();
-//        if (searchAdapter != null)
-//            searchAdapter.stopListening();
-//        super.onStop();
-//    }
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        if (adapter != null)
-//            adapter.startListening();
-//        if (searchAdapter != null)
-//            searchAdapter.startListening();
-//    }
-//
-//    private void startSearch(String search_value) {
-//        List<String> lstUserEmail = new ArrayList<>();
-//        Query query = FirebaseDatabase.getInstance()
-//                .getReference(Common.USER_INFORMATION)
-//                .child(Common.loggedUser.getUid())
-//                .child(Common.ACCEPT_LIST)
-//                .orderByChild("name")
-//                .startAt(search_value);
-//
-//        FirebaseRecyclerOptions<User> options = new FirebaseRecyclerOptions.Builder<User>()
-//                .setQuery(query,User.class)
-//                .build();
-//
-//        searchAdapter = new FirebaseRecyclerAdapter<User, UserViewHolder>(options) {
-//            @Override
-//            protected void onBindViewHolder(@NonNull UserViewHolder holder, int position, @NonNull final User model) {
-//                holder.txt_user_email.setText(new StringBuilder(model.getEmail()));
-//
-//                holder.setiRecyclerItemClickListener(new IRecyclerItemClickListener() {
-//                    @Override
-//                    public void onItemClickListener(View view, int position) {
-//                        Common.trackingUser = model;
-//                        startActivity(new Intent(HomeActivity.this,TrackingActivity.class));
-//                    }
-//                });
-//            }
-//
-//            @NonNull
-//            @Override
-//            public UserViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-//                View itemView = LayoutInflater.from(viewGroup.getContext())
-//                        .inflate(R.layout.layout_user,viewGroup,false);
-//                return new UserViewHolder(itemView);
-//            }
-//        };
-//        searchAdapter.startListening();;
-//        recycler_friend_list.setAdapter(searchAdapter);
-//    }
-//
-    @Override
-    public void onFirebaseLoadUserNameDone(List<String> lstEmail) {
-        searchBar.setLastSuggestions(lstEmail);
-    }
-
-    @Override
-    public void onFirebaseLoadFailed(String message) {
-
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-
-    }
 }
