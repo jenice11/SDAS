8d94395179ab981501f68d790f44ee43d65d31cb
jenice11 <38397984+jenice11@users.noreply.github.com>
2021-11-16 20:09:58 +0800

    0.1.1
    Current functions:
    Registration
    Login
    Firebase
    Location upload
    
    Work in progress:
    Retrieving data from firebase

--- app/src/main/java/com/example/trackingapp/HomeActivity.java
+++ app/src/main/java/com/example/trackingapp/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.trackingapp;
 import static android.content.ContentValues.TAG;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
@@ -15,11 +16,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.trackingapp.Interface.IFirebaseLoadDone;
@@ -28,6 +31,7 @@ import com.example.trackingapp.Model.User;
 import com.example.trackingapp.Service.TrackingService;
 import com.example.trackingapp.Utils.Common;
 import com.example.trackingapp.ViewHolder.UserViewHolder;
+import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -36,12 +40,21 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.maps.android.SphericalUtil;
+
+import org.w3c.dom.Comment;
+
+import java.util.ArrayList;
+
+import io.paperdb.Paper;
 
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
@@ -57,13 +70,15 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     Double distance;
     DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
-
+    private static final int MY_REQUEST_CODE = 7117;
+    String userID;
 
     /////////////
 
     FirebaseRecyclerAdapter<User, UserViewHolder> adapter, searchAdapter;
     RecyclerView recycler_friend_list;
     IFirebaseLoadDone firebaseLoadDone;
+    DatabaseReference user_information;
 
 //    MaterialSearchBar searchBar;
 
@@ -118,6 +133,52 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+        Paper.init(this);
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        user_information = FirebaseDatabase.getInstance().getReference(Common.USER_INFORMATION);
+        user_information.keepSynced(true);
+
+    }
+
+    //Testing firebase get data
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == MY_REQUEST_CODE)
+        {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(resultCode == RESULT_OK){
+                final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                user_information.orderByKey()
+                        .equalTo(firebaseUser.getUid())
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.getValue() == null)
+                                {
+                                    if(!dataSnapshot.child(firebaseUser.getUid()).exists())
+                                    {
+                                        Common.loggedUser = new User(firebaseUser.getUid(),firebaseUser.getEmail());
+
+                                        user_information.child(Common.loggedUser.getUid())
+                                                .setValue(Common.loggedUser);
+                                    }
+                                }
+
+                                else{
+                                    Common.loggedUser = dataSnapshot.child(firebaseUser.getUid()).getValue(User.class);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+            }
+        }
     }
 
     public void onClick(View v) {
@@ -141,6 +202,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(this, "Tracking stopped", Toast.LENGTH_SHORT).show();
         }
     }
+
+
 
 
 
