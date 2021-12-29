ce949af9e50c1713a83863e295aa6093c239a2cd
jenice11 <38397984+jenice11@users.noreply.github.com>
2021-12-29 21:38:09 +0800

    Update MyLocationReceiver.java
    Experimental checking duplicate history much broken loop

--- app/src/main/java/com/example/sdas/Service/MyLocationReceiver.java
+++ app/src/main/java/com/example/sdas/Service/MyLocationReceiver.java
@@ -8,34 +8,26 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
 import android.preference.PreferenceManager;
-import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import com.esotericsoftware.kryo.NotNull;
 import com.example.sdas.Model.History;
 import com.example.sdas.Model.MyLocation;
 import com.example.sdas.Utils.Common;
 import com.google.android.gms.location.LocationResult;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
-
-import org.joda.time.DateTime;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 import io.paperdb.Paper;
 
@@ -51,8 +43,7 @@ public class MyLocationReceiver extends BroadcastReceiver {
     private SharedPreferences mPreferences;
     private SharedPreferences.Editor mEditor;
     long ct = System.currentTimeMillis();
-
-
+    Boolean checklist;
     DatabaseReference publicLocation = FirebaseDatabase.getInstance().getReference(Common.PUBLIC_LOCATION);
 
     @Override
@@ -96,15 +87,11 @@ public class MyLocationReceiver extends BroadcastReceiver {
 
     //start comment
     public void getDistance(){
-//        System.out.println("How many times it fking loop bitch");
-
         trackingUserLocation = FirebaseDatabase.getInstance().getReference(Common.PUBLIC_LOCATION);
 //        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         trackingUserLocation.orderByKey()
                 //.equalTo(firebaseUser.getUid())
                 .addListenerForSingleValueEvent(new ValueEventListener() {
-                    double LogLatA, LogLonA, LogLatB, LogLonB;
-                    String LogDate, LogTime;
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         if(dataSnapshot.exists())
@@ -135,15 +122,15 @@ public class MyLocationReceiver extends BroadcastReceiver {
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        String abc = snapshot.getKey();
+//                        String abc = snapshot.getKey();
 
-                        Log.d("what key===", abc);
+//                        Log.d("what key===", abc);
                         MyLocation currentlocation = snapshot.getValue(MyLocation.class);
 
                         double latA = (double) currentlocation.getLatitude();
                         double longA = (double) currentlocation.getLongitude();
                         System.out.println("PLACEHOLDER");
-                        System.out.println("A lat  " + latA + "A long "+ longA);
+//                        System.out.println("A lat  " + latA + "A long "+ longA);
 
                         double latitudeB=0, longitudeB=0, x = 0, y = 0;
 
@@ -163,12 +150,12 @@ public class MyLocationReceiver extends BroadcastReceiver {
 
                                         x = Double.valueOf(location.getLatitude());
                                         y = Double.valueOf(location.getLongitude());
-
-                                        System.out.println("B lat  " + latitudeB + "B long "+ longitudeB);
-                                        System.out.println("x lat  " + x + "x long "+ y);
-
-                                        Log.d(TAG, "<<4 == LatA= " + latA + "LongA= " + longA);
-                                        Log.d(TAG, "<<4 == LatB= " + latitudeB + "LongB= " + longitudeB);
+//
+//                                        System.out.println("B lat  " + latitudeB + "B long "+ longitudeB);
+//                                        System.out.println("x lat  " + x + "x long "+ y);
+//
+//                                        Log.d(TAG, "<<4 == LatA= " + latA + "LongA= " + longA);
+//                                        Log.d(TAG, "<<4 == LatB= " + latitudeB + "LongB= " + longitudeB);
 
                                         distance = calculateDistance(latA,longA, latitudeB, longitudeB);
 
@@ -181,41 +168,161 @@ public class MyLocationReceiver extends BroadcastReceiver {
                                         SimpleDateFormat stf = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
                                         String time = stf.format(Calendar.getInstance().getTime());
                                         String risk = "No Risk";
+                                        if(distance<=0.5 && distance >=0){ risk = "High"; }
+                                        if(distance<=1.0 && distance >=0.5){ risk = "Medium"; }
+                                        if(distance<=1.5 && distance >=1.0){ risk = "Low"; }
 
-                                        if(distance <= 1.5){
-                                            if(distance<=0.5 && distance >=0){ risk = "High"; }
-                                            if(distance<=1.0 && distance >=0.5){ risk = "Medium"; }
-                                            if(distance<=1.5 && distance >=1.0){ risk = "Low"; }
-
-                                            History history = new History();
-
-
-
-                                            history.setDistance(distance);
-                                            history.setDate(date);
-                                            history.setTime(time);
-                                            history.setLatitudeA(latA);
-                                            history.setLongitudeA(longA);
-                                            history.setLatitudeB(latitudeB);
-                                            history.setLongitudeB(longitudeB);
-                                            history.setRisk(risk);
-//                                            history.setTimestamp(history.getTimestampLong());
-
-                                            history.setTimestamp(ct);
-
-                                            mEditor.putLong("currentTime", ct);
-                                            mEditor.commit();
-
-                                            System.out.println("11Shared current time = " + ct);
+                                        DatabaseReference user_history = FirebaseDatabase.getInstance().getReference(Common.HISTORY).child(Common.loggedUser.getUid());
+                                        //Yes its working for 1 variable only
+//                                        user_history.orderByChild("distance").equalTo(distance)
+//                                                .addListenerForSingleValueEvent(new ValueEventListener() {
+//                                                    @Override
+//                                                    public void onDataChange(DataSnapshot dataSnapshot) {
+//                                                        if(dataSnapshot.exists()){
+//                                                            System.out.println("Data exist !!!!!");
+//                                                            System.out.println(dataSnapshot);
+//                                                        } else {
+//                                                            System.out.println("Data not exist xxx");
+//                                                            System.out.println(dataSnapshot);
+//                                                        }
+//                                                    }
+//
+//                                                    @Override
+//                                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+//                                                        throw databaseError.toException(); // don't ignore errors
+//                                                    }
+//                                                });
 
 
 
-                                            listhistory.setValue(history);
+                                        double LatitudeB2 = latitudeB;
+                                        double LongitudeB2 = longitudeB;
+                                        String risk2 = risk;
+
+                                        if(distance >=0 && distance <= 1.5) {
+                                            user_history.orderByChild("timestamp").limitToLast(1).addListenerForSingleValueEvent(new ValueEventListener() {
+                                                boolean processDone = false;
+                                                @Override
+                                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                    if(!dataSnapshot.exists()) {
+                                                        if (distance <= 1.5) {
+                                                            History history = new History();
+
+                                                            history.setDistance(distance);
+                                                            history.setDate(date);
+                                                            history.setTime(time);
+                                                            history.setLatitudeA(latA);
+                                                            history.setLongitudeA(longA);
+                                                            history.setLatitudeB(LatitudeB2);
+                                                            history.setLongitudeB(LongitudeB2);
+                                                            history.setRisk(risk2);
+                                                            history.setTimestamp(ct);
+
+                                                            mEditor.putLong("currentTime", ct);
+                                                            mEditor.commit();
+
+//                                                            System.out.println("11Shared current time = " + ct);
+
+                                                            listhistory.setValue(history);
+                                                            Log.w(TAG, ">>>> new history added, no exist");
+                                                        }
+                                                    }
+                                                    for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                                                        if(dataSnapshot1.exists()){
+                                                            System.out.println(dataSnapshot1);
+                                                            if (dataSnapshot1.child("latitudeA").getValue().equals(latA)
+                                                                    && dataSnapshot1.child("longitudeA").getValue().equals(longA)
+                                                                    && dataSnapshot1.child("latitudeB").getValue().equals(LatitudeB2)
+                                                                    && dataSnapshot1.child("longitudeB").getValue().equals(LongitudeB2)
+                                                                    && dataSnapshot1.child("distance").getValue().equals(distance)
+                                                            ) {
+//                                                        System.out.println("Distance got = " + distance);
+//                                                        System.out.println("latA got = " + latA);
+//                                                        System.out.println("longA got = " + longA);
+//                                                        System.out.println("latitudeB got = " + finalLatitudeB);
+//                                                        System.out.println("longitudeB got = " + finalLongitudeB);
+//                                                        System.out.println("------------------------------");
+
+                                                                long checktime, time1;
+                                                                long datatime = (long) dataSnapshot1.child("timestamp").getValue();
+
+                                                                checktime = ct - datatime;
+                                                                System.out.println("Data Time = " + datatime + "  / Check time = " + checktime);
+
+                                                                System.out.println("This date = " + dataSnapshot1.child("date").getValue() + "Bo Date = " + date);
+
+                                                                if ((dataSnapshot1.child("date").getValue().equals(date) && checktime >= 600000)) {
+                                                                    System.out.println("Time >>> than 10 minutes on same day");
+
+                                                                    History history = new History();
+
+                                                                    history.setDistance(distance);
+                                                                    history.setDate(date);
+                                                                    history.setTime(time);
+                                                                    history.setLatitudeA(latA);
+                                                                    history.setLongitudeA(longA);
+                                                                    history.setLatitudeB(LatitudeB2);
+                                                                    history.setLongitudeB(LongitudeB2);
+                                                                    history.setRisk(risk2);
+                                                                    history.setTimestamp(ct);
+
+                                                                    mEditor.putLong("currentTime", ct);
+                                                                    mEditor.commit();
+
+                                                                    listhistory.setValue(history);
+                                                                    Log.w(TAG, ">>>>>>>>>new history added same date");
 
 
+                                                                } else {
+                                                                    System.out.println("Time xx << 10 minutes");
+                                                                }
+                                                        }
 
+                                                        } else {
+                                                            System.out.println("Data not exist yet xxx");
+                                                        System.out.println(dataSnapshot1);
+                                                            System.out.println(processDone);
 
+                                                            if (!processDone){
+                                                                if (distance <= 1.5) {
+                                                                    History history = new History();
+
+                                                                    history.setDistance(distance);
+                                                                    history.setDate(date);
+                                                                    history.setTime(time);
+                                                                    history.setLatitudeA(latA);
+                                                                    history.setLongitudeA(longA);
+                                                                    history.setLatitudeB(LatitudeB2);
+                                                                    history.setLongitudeB(LongitudeB2);
+                                                                    history.setRisk(risk2);
+                                                                    history.setTimestamp(ct);
+
+                                                                    mEditor.putLong("currentTime", ct);
+                                                                    mEditor.commit();
+
+//                                                            System.out.println("11Shared current time = " + ct);
+
+                                                                    listhistory.setValue(history);
+                                                                    Log.w(TAG, ">>>> new history added, no exist");
+                                                                }
+                                                                processDone = true;
+//                                                            System.out.println("2 " + processDone);
+//                                                                user_history.orderByChild("timestamp").removeEventListener(this);
+
+                                                            }
+
+                                                        }
+                                                    }
+                                                }
+
+                                                @Override
+                                                public void onCancelled(@NonNull DatabaseError databaseError) {
+                                                    user_history.removeEventListener(this);
+                                                }
+                                            });
                                         }
+
+
                                     }
                                 }
                                 else
